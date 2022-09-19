63e0821a014e4a55c4330bf1c909842870203379
LeoDoesThings <leodoesthings@yahoo.com>
2022-09-20 11:44:54 +1200

    Add form functionality
    - Add ability to enter delivery info
    - Add form selection functionality
    - Minimalise code

--- script.js
+++ script.js
@@ -12,17 +12,18 @@ const COFFEE = [
   "Short Black",
   "Piccolo Latte",
 ];
+const maxCapacity = 10;
 const costRegular = 5.5;
 const costMedium = costRegular + 1;
 const costLarge = costRegular + 2;
 const costShipping = 5;
+let costTotal = 0;
 
-let orders = {};
 let orderRows = "";
 let coffeeOptions = "";
-COFFEE.forEach((name) => generateCoffeeOption(name));
+COFFEE.forEach(createCoffeeOption);
 
-// Number formatter for converting to dollars in the US format
+// Formatter for converting numbers to dollars in the US format
 var currency = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
@@ -36,23 +37,34 @@ orderForm.addEventListener("submit", function (e) {
 function newOrder() {
   let orderBody = document.getElementById("orderBody");
   orderRows = "";
-  for (let i = 0; i < 10; i++) {
-    createCoffeeRow(COFFEE[i], i);
+  orderTotal = `<th scope="row" class="fw-bold">Total</th>
+      <td></td><td></td>
+      <td class="text-end" id="costTotal">
+        ${currency.format(costTotal)}
+      </td>
+    </tr>`;
+  for (let i = 0; i < maxCapacity; i++) {
+    createCoffeeRow(i);
   }
-  orderBody.innerHTML = orderRows;
+  orderTable = orderRows + orderTotal;
+  orderBody.innerHTML = orderTable;
+  for (let i = 0; i < maxCapacity; i++) {
+    updateCoffee(i);
+  }
   showPage(orderMenu);
 }
 
-function createCoffeeRow(name, index) {
+function createCoffeeRow(index) {
   orderRows += `<tr>
       <th scope="row" class="fw-normal">${index + 1}.</th>
       <td>
         <select
+          onchange="updateCoffee(${index})"
           class="form-select"
           name="coffeeDropdown"
           id="coffeeDropdown${index}"
         >
-          <option selected>None</option>
+          <option value="none" selected>None</option>
           ${coffeeOptions}
         </select>
       </td>
@@ -62,29 +74,72 @@ function createCoffeeRow(name, index) {
           class="form-select"
           name="sizeDropdown"
           id="sizeDropdown${index}"
+          disabled
         >
           <option value="${costRegular}">Regular</option>
           <option value="${costMedium}">Medium</option>
           <option value="${costLarge}">Large</option>
         </select>
       </td>
-      <td class="text-end" id="cost${index}">
+      <td class="text-end" id="cost-${index}">
         ${currency.format(0)}
       </td>
     </tr>`;
 }
 
-function generateCoffeeOption(name) {
+function createCoffeeOption(name, index) {
   // .replace(/\s+/g, "") will remove all whitespaces in the string
-  coffeeOptions += `<option value="${name.replace(/\s+/g, "")}">${name}
+  coffeeOptions += `<option value="coffee${index}">${name}
                     </option>`;
 }
 
+function updateCoffee(index) {
+  let coffeeElement = document.getElementById(`coffeeDropdown${index}`);
+  let sizeElement = document.getElementById(`sizeDropdown${index}`);
+  if (coffeeElement.value != "none") {
+    sizeElement.disabled = false;
+    sizeElement.classList.add("bg-danger");
+  } else {
+    sizeElement.value = "";
+    sizeElement.classList.remove("bg-danger");
+    sizeElement.disabled = true;
+  }
+  updateCost(index);
+}
+
 function updateCost(index) {
-  let costElement = document.getElementById(`cost${index}`);
-  // Use parseFloat to convert string to number and round to 2dp
-  let cost = document.getElementById(`sizeDropdown${index}`).value;
+  let coffeeElement = document.getElementById(`coffeeDropdown${index}`);
+  let sizeElement = document.getElementById(`sizeDropdown${index}`);
+  let costElement = document.getElementById(`cost-${index}`);
+  let costTotalElement = document.getElementById("costTotal");
+  let cost = 0;
+  if (sizeElement.value != "") {
+    sizeElement.classList.remove("bg-danger");
+  }
+  // Run if the user has selected a coffee
+  if (coffeeElement.value != "none") {
+    // Get the cost of the new size selected
+    cost = document.getElementById(`sizeDropdown${index}`).value;
+  }
+  // Inject new cost into the element
   costElement.innerHTML = `${currency.format(cost)}`;
+  // Update total cost
+  let costElements = document.querySelectorAll(`td[id^="cost-"]`);
+  costTotal = 0;
+  costElements.forEach(
+    (element) =>
+      (costTotal += Number(element.textContent.replace(/[^0-9\.]+/g, "")))
+  );
+  costTotalElement.innerHTML = currency.format(costTotal);
+}
+
+function updateTransport(transportSelect) {
+  let deliveryElement = document.getElementById("deliveryInfo");
+  if (transportSelect.value == "delivery") {
+    deliveryElement.classList.remove("d-none");
+  } else {
+    deliveryElement.classList.add("d-none");
+  }
 }
 
 function checkEmpty(userInput) {
@@ -109,6 +164,8 @@ function showPage(page) {
 
 function resetOutput() {
   output.classList.add("d-none");
+  output.classList.remove("alert-danger");
+  output.classList.remove("alert-success");
   output.innerHTML = "";
 }
 

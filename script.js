7b6b4406a0c63d1d0ab100bf7c10dbba33be080b
LeoDoesThings <leodoesthings@yahoo.com>
2022-10-04 16:57:22 +1300

    Complete Invoice Page
    - Completed invoice page with `readOrder` function
    - Update comments
    - Update function names

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ let remainingCapacity = maxCapacity;
 
 let customerInfo = [];
 let order = [];
+let invoiceRows = "";
 let orderRows = "";
 let coffeeOptions = "";
 COFFEE.forEach(createCoffeeOption);
@@ -40,12 +41,11 @@ orderForm.addEventListener("submit", function (e) {
   let customerInputs = document.querySelectorAll(`input[id^="customer"]`);
   customerInputs.forEach(function (input) {
     input = input.value;
-    if (checkEmpty(input)) {
-      customerInfo.push(input.value);
-    } else {
+    if (isEmpty(input)) {
       alert("red", "Please enter all customer information");
       run = false;
-      return;
+    } else {
+      customerInfo.push(input.value);
     }
   });
 
@@ -79,8 +79,10 @@ orderForm.addEventListener("submit", function (e) {
   if (orderSum == 0) {
     alert("red", "Please add at least 1 coffee to the order");
   } else {
+    readOrder(order, true);
     showPage(orderInvoice);
   }
+
   console.log(order);
 });
 
@@ -97,6 +99,7 @@ function newOrder() {
     Regular - ${currency.format(costRegular)}<br>
     Medium - ${currency.format(costMedium)}<br>
     Large - ${currency.format(costLarge)}`;
+  // Set HTML rows for costs
   orderShipping = `
     <tr id="rowDelivery" class="d-none">
     <th scope="row" class="fw-bold">Delivery</th>
@@ -113,19 +116,24 @@ function newOrder() {
         ${currency.format(costTotal)}
       </td>
     </tr>`;
+  // Create the maximum amount of input rows
   for (let i = 0; i < maxCapacity; i++) {
     createCoffeeRow(i);
   }
+  // Inject HTML into the page
   orderTable = orderRows + orderShipping + orderTotal;
   orderBody.innerHTML = orderTable;
+  // Clear all automatic selections from browser
   for (let i = 0; i < maxCapacity; i++) {
     updateCoffee(i);
   }
+  // Inject HTML and show the page
   bulkCoffeeSelect.innerHTML = coffeeOptions;
   showPage(orderMenu);
 }
 
 function createCoffeeRow(index) {
+  // Create a row of inputs with IDs depending on index number
   orderRows += `
     <tr id="coffeeRow${index}">
       <th scope="row" class="fw-normal">${index + 1}.</th>
@@ -160,7 +168,6 @@ function createCoffeeRow(index) {
 }
 
 function createCoffeeOption(name, index) {
-  // .replace(/\s+/g, "") will remove all whitespaces in the string
   coffeeOptions += `<option value="${index}">${name}
                     </option>`;
 }
@@ -168,7 +175,7 @@ function createCoffeeOption(name, index) {
 function updateCoffee(index) {
   let coffeeElement = document.getElementById(`coffeeDropdown${index}`);
   let sizeElement = document.getElementById(`sizeDropdown${index}`);
-  // If a coffee is selected, inform the user of the empty size
+  // If a coffee is selected, indicate empty input for size by setting a red background
   if (coffeeElement.value != "none") {
     sizeElement.disabled = false;
     sizeElement.classList.add("bg-danger");
@@ -185,6 +192,7 @@ function updateCost(index) {
   let sizeElement = document.getElementById(`sizeDropdown${index}`);
   let costElement = document.getElementById(`cost-${index}`);
   let cost = 0;
+  // If a size is inputted then remove indicator of empty input
   if (sizeElement.value != "") {
     sizeElement.classList.remove("bg-danger");
   }
@@ -217,6 +225,7 @@ function updateTransport(transportSelect) {
   let deliveryElement = document.getElementById("deliveryInfo");
   let deliveryRow = document.getElementById("rowDelivery");
   if (transportSelect.value == "delivery") {
+    // If order is delivery then show inputs for delivery information
     isDelivery = true;
     deliveryElement.innerHTML = `
       <div class="input-group mb-2">
@@ -233,50 +242,59 @@ function updateTransport(transportSelect) {
       </div>`;
     deliveryRow.classList.remove("d-none");
   } else {
+    // If order is not delivery then do not show inputs for delivery information
     isDelivery = false;
     deliveryElement.innerHTML = "";
     deliveryRow.classList.add("d-none");
   }
+  // Add or remove the delivery fee depending on the value of `isDelivery`
   updateTotal();
 }
 
-function updateBulk(bulkSelect) {
+// This function is run when the user changes item
+function updateBulkName(bulkSelect) {
+  // Update the item name when user changes item
   let bulkCoffeeName = document.getElementById("bulkCoffeeName");
   bulkCoffeeName.innerHTML = COFFEE[bulkSelect.value];
 }
 
 function verifyAmount(input) {
   if (input.value < 1) {
+    // If user inputs an amount smaller than 1 then reset amount to 1
     input.value = 1;
     alert("red", "You must add at least 1 item");
   } else if (input.value > remainingCapacity) {
+    // If user inputs an amount larger than remaining capacity then reset amount to remaining capacity
     input.value = remainingCapacity;
     alert("red", `The remaining capacity is ${remainingCapacity} items`);
   }
 }
 
 function bulkAdd() {
+  // Do not run if there is no capacity remaining
   if (remainingCapacity <= 0) {
     alert("red", "No capacity remaining. No items added.");
     return;
   }
   let amount = document.getElementById("bulkAddAmount").value;
-  // If no amount was selected then do not add anything
+  // Do not run if an amount was not specified
   if (amount == "") {
     alert("red", "No amount entered. Added 0 items.");
     return;
   }
-  let size = document.getElementById("bulkAddSize").value;
+  // Do not run if no item was specified
   let coffeeSelect = document.getElementById("bulkCoffeeSelect").value;
   if (coffeeSelect == "") {
     alert("red", "Select a coffee to add");
     return;
   }
+  // Get HTML elements
+  let size = document.getElementById("bulkAddSize").value;
   let formCoffees = document.querySelectorAll("select[id^='coffeeDropdown']");
   let formSizes = document.querySelectorAll("select[id^='sizeDropdown']");
   let formCoffeesEmpty = [];
   let formSizesEmpty = [];
-  // Store all empty coffee and size inputs in main form in respective arrays
+  // Store all empty coffee and size inputs from main form in respective arrays
   formCoffees.forEach(function (coffeeElement) {
     if (coffeeElement.value == "none") {
       formCoffeesEmpty.push(coffeeElement);
@@ -287,7 +305,7 @@ function bulkAdd() {
       formSizesEmpty.push(sizeElement);
     }
   });
-  // Add the bulk items to empty main form rows
+  // Add the bulk items to empty inputs
   for (let i = 0; i < amount; i++) {
     formCoffeesEmpty[i].value = coffeeSelect;
     formSizesEmpty[i].value = size;
@@ -297,25 +315,97 @@ function bulkAdd() {
   // Clear bulk amount input field
   document.getElementById("bulkAddAmount").value = "";
   let sizeCapitalised = size.charAt(0).toUpperCase() + size.slice(1);
+  // Inform user of successful action with details
   alert("green", `Added ${amount}x ${sizeCapitalised} ${COFFEE[coffeeSelect]}`);
 }
 
 function calculateCapacity() {
   let coffeeSizes = document.querySelectorAll("select[id^='sizeDropdown']");
+  // Reset remaining capacity and for every coffee selected, remove 1 from remaining capacity
   remainingCapacity = maxCapacity;
   coffeeSizes.forEach(function (size) {
-    if (checkEmpty(size.value)) {
+    if (!isEmpty(size.value)) {
       remainingCapacity -= 1;
     }
   });
 }
 
-function checkEmpty(userInput) {
+function readOrder(order, canEdit) {
+  // Get HTML elements
+  let invoiceBody = document.getElementById("invoiceBody");
+  let title = document.getElementById("invoiceTitle");
+  // Show order title
+  if (isDelivery == true) title.innerHTML = "Delivery Order";
+  else title.innerHTML = "Pick-up Order";
+  // Reset variables
+  let costTotal = 0;
+  let size;
+  invoiceRows = "";
+
+  // Change function of back button if the order can not be edited
+  if (canEdit == false) {
+    let editOrderBtn = document.getElementById("editOrder");
+    editOrderBtn.innerHTML = "&larr; Return to Saved Orders Page";
+    editOrderBtn.onclick = showPage(savedOrders);
+  }
+
+  // Read order array and show as HTML table
+  for (let i = 0; i < COFFEE.length; i++) {
+    for (let p = 0; p < order[p].length; p++) {
+      let amount = order[i][p];
+      if (amount > 0) {
+        if (p == 2) {
+          size = "Large";
+          cost = costLarge;
+        } else if (p == 1) {
+          size = "Medium";
+          cost = costMedium;
+        } else {
+          size = "Regular";
+          cost = costRegular;
+        }
+        cost = cost * amount;
+        costTotal += cost;
+        invoiceRows += `
+          <tr>
+            <td>${COFFEE[i]}</td>
+            <td>${size}</td>
+            <td>${amount}x</td>
+            <td class="text-end">
+              ${currency.format(cost)}
+            </td>
+          </tr>`;
+      }
+    }
+  }
+  if (isDelivery == true) {
+    orderShipping = `
+      <tr>
+      <th scope="row" class="fw-bold">Delivery</th>
+        <td></td><td></td>
+        <td class="text-end" id="costShipping">
+          ${currency.format(costShipping)}
+        </td>
+      </tr>`;
+    costTotal += costShipping;
+  }
+  orderTotal = `
+    <tr>
+      <th scope="row" class="fw-bold">Total</th>
+      <td></td><td></td>
+      <td class="text-end">
+        ${currency.format(costTotal)}
+      </td>
+    </tr>`;
+  invoiceBody.innerHTML = invoiceRows + orderShipping + orderTotal;
+}
+
+function isEmpty(userInput) {
   // Check if input is empty
   if (userInput == "") {
-    return false;
-  } else {
     return true;
+  } else {
+    return false;
   }
 }
 
